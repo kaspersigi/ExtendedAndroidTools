52ada7dd46e828bc24491ef28a64fd3030a45607
kaspersigi <kaspersigi@outlook.com>
2026-02-20 09:32:00 +0800

    update NDK to r27d

--- scripts/download-ndk.sh
+++ scripts/download-ndk.sh
@@ -2,12 +2,12 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
 dest="${1:-$(pwd)}"
-TMP_NDK="/tmp/android-ndk-r27b-linux.zip"
+TMP_NDK="/tmp/android-ndk-r27d-linux.zip"
 
 echo "downloading ndk to ${dest}..."
 
-curl -s -o "${TMP_NDK}" https://dl.google.com/android/repository/android-ndk-r27b-linux.zip
+curl -s -o "${TMP_NDK}" https://dl.google.com/android/repository/android-ndk-r27d-linux.zip
 unzip -q "${TMP_NDK}" -d "${dest}"
-rm /tmp/android-ndk-r27b-linux.zip
+rm /tmp/android-ndk-r27d-linux.zip
 
 echo "done"
