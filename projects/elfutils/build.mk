abd1d892d202861927bc6f5793badba769b31424
kaspersigi <kaspersigi@outlook.com>
2026-03-15 23:58:23 +0800

    elfutils: add --with-zlib to fix crc32 duplicate symbol error
    Prevents libelf from exporting its own crc32 implementation, which
    conflicts with libz.a when statically linking bpftrace.
    
    Adds:
      - --with-zlib=$(ANDROID_OUT_DIR)
      - LDFLAGS="-L$(ANDROID_OUT_DIR)/lib"
    
    libelf now uses zlib's crc32 internally without symbol collision.

--- projects/elfutils/build.mk
+++ projects/elfutils/build.mk
@@ -21,7 +21,9 @@ $(ANDROID_BUILD_DIR)/elfutils: $(ANDROID_OUT_DIR)/lib/pkgconfig/zlib.pc
 		$(ANDROID_EXTRA_CONFIGURE_FLAGS) \
 		--disable-debuginfod \
 		--disable-libdebuginfod \
-		--enable-install-elfh
+		--enable-install-elfh \
+		--with-zlib=$(ANDROID_OUT_DIR) \
+		LDFLAGS="-L$(ANDROID_OUT_DIR)/lib"
 
 ELFUTILS_VERSION = 0.194
 ELFUTILS_URL = http://sourceware.org/pub/elfutils/$(ELFUTILS_VERSION)/elfutils-$(ELFUTILS_VERSION).tar.bz2
