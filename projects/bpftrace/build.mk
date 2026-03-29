c5e7c0ff7d63f5aac12bbb4ee687e5420a5a3f89
kaspersigi <kaspersigi@outlook.com>
2026-03-29 20:29:53 +0800

    update bpftrace from 0.25.0 to 0.25.1

--- projects/bpftrace/build.mk
+++ projects/bpftrace/build.mk
@@ -46,10 +46,10 @@ $(STRIP_THUNK): projects/bpftrace/strip-thunk | $(HOST_OUT_DIR)
 	@sed -e "s+<STRIP_PATH>+$(ANDROID_TOOLCHAIN_STRIP_PATH)+g" $< > $@
 	chmod +x $@
 
-BPFTRACE_COMMIT = v0.25.0
+BPFTRACE_COMMIT = v0.25.1
 BPFTRACE_REPO = https://github.com/iovisor/bpftrace.git/
 projects/bpftrace/sources:
 	git clone $(BPFTRACE_REPO) $@ && \
 	cd $@ && \
 	git checkout $(BPFTRACE_COMMIT) && \
-	{ git apply --verbose ../tracefs_fallback_v0.25.0.patch || (echo "Patch failed to apply, stopping build!" && exit 1); }
+	{ git apply --verbose ../tracefs_fallback_v0.25.1.patch || (echo "Patch failed to apply, stopping build!" && exit 1); }
